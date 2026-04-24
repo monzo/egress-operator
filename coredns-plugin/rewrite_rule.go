3aaeb128b1278cb1a61229ff1589f4d50f7393db
Aaron <aarontaylor@monzo.com>
2026-04-24 16:00:35 +0100

    Upgrade coredns to 1.13.2

--- coredns-plugin/rewrite_rule.go
+++ coredns-plugin/rewrite_rule.go
@@ -2,32 +2,46 @@ package egressoperator
 
 import (
 	"context"
+	"regexp"
 
 	"github.com/coredns/coredns/plugin/rewrite"
 	"github.com/coredns/coredns/request"
+	"github.com/miekg/dns"
 )
 
 type exactNameRule struct {
 	NextAction string
 	From       string
 	To         string
-	rewrite.ResponseRule
+
+	AnswerPattern     *regexp.Regexp
+	AnswerReplacement string
 }
 
-var _ rewrite.Rule = &exactNameRule{}
+var (
+	_ rewrite.Rule         = &exactNameRule{}
+	_ rewrite.ResponseRule = &exactNameRule{}
+)
 
 // Rewrite rewrites the current request based upon exact match of the name
 // in the question section of the request.
-func (rule *exactNameRule) Rewrite(ctx context.Context, state request.Request) rewrite.Result {
+func (rule *exactNameRule) Rewrite(ctx context.Context, state request.Request) (rewrite.ResponseRules, rewrite.Result) {
 	if rule.From == state.Name() {
 		state.Req.Question[0].Name = rule.To
-		return rewrite.RewriteDone
+		return rewrite.ResponseRules{rule}, rewrite.RewriteDone
 	}
-	return rewrite.RewriteIgnored
+	return nil, rewrite.RewriteIgnored
 }
 
 // Mode returns the processing nextAction
 func (rule *exactNameRule) Mode() string { return rule.NextAction }
 
-// GetResponseRule return a rule to rewrite the response with. Currently not implemented.
-func (rule *exactNameRule) GetResponseRule() rewrite.ResponseRule { return rule.ResponseRule }
+// RewriteResponse rewrites the Name of response records matching AnswerPattern
+// back to the original question name, so clients see an answer for what they
+// asked rather than the internal egress-gateway service name.
+func (rule *exactNameRule) RewriteResponse(res *dns.Msg, rr dns.RR) {
+	h := rr.Header()
+	if rule.AnswerPattern.MatchString(h.Name) {
+		h.Name = rule.AnswerPattern.ReplaceAllString(h.Name, rule.AnswerReplacement)
+	}
+}
